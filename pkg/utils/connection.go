78797176f00578575092e331f6512406413ebb6f
thelicato <thelicato@users.noreply.github.com>
2024-10-22 10:13:56 +0200

    feat: improve connection for multiline text

--- pkg/utils/connection.go
+++ pkg/utils/connection.go
@@ -1,20 +1,18 @@
 package utils
 
 import (
-	"bufio"
-	"bytes"
+	"context"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"os"
 	"sync"
-	"time"
 
 	"golang.design/x/clipboard"
 )
 
 func HandleConnection(conn io.ReadWriteCloser) {
 	var wg sync.WaitGroup
-	clipboardCh := make(chan []byte)
 
 	// Initialize the clipboard package
 	err := clipboard.Init()
@@ -23,62 +21,80 @@ func HandleConnection(conn io.ReadWriteCloser) {
 		os.Exit(1)
 	}
 
-	wg.Add(3)
-	go monitorClipboard(clipboardCh, &wg)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Start clipboard monitoring
+	clipboardCh := clipboard.Watch(ctx, clipboard.FmtText)
+
+	wg.Add(2)
 	go readFromConnection(conn, &wg)
 	go writeToConnection(conn, clipboardCh, &wg)
 
 	wg.Wait()
 }
 
-func monitorClipboard(ch chan<- []byte, wg *sync.WaitGroup) {
-	defer wg.Done()
-	var lastText []byte
-	for {
-		text := clipboard.Read(clipboard.FmtText)
-		if text == nil {
-			time.Sleep(time.Second)
-			continue
-		}
-		if !bytes.Equal(text, lastText) {
-			// Make a copy of text to avoid data race
-			lastText = make([]byte, len(text))
-			copy(lastText, text)
-			ch <- text
-		}
-		time.Sleep(500 * time.Millisecond)
-	}
-}
-
 func readFromConnection(conn io.Reader, wg *sync.WaitGroup) {
 	defer wg.Done()
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		data := scanner.Bytes()
+	for {
+		// Read 4-byte length header
+		header := make([]byte, 4)
+		_, err := io.ReadFull(conn, header)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error reading header from connection:", err)
+			}
+			return
+		}
+		length := binary.BigEndian.Uint32(header)
+		if length == 0 {
+			continue
+		}
+		// Read the data
+		data := make([]byte, length)
+		_, err = io.ReadFull(conn, data)
+		if err != nil {
+			fmt.Println("Error reading data from connection:", err)
+			return
+		}
 		currentClipboard := clipboard.Read(clipboard.FmtText)
-		if !bytes.Equal(data, currentClipboard) {
+		if !equalBytes(data, currentClipboard) {
 			clipboard.Write(clipboard.FmtText, data)
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading from connection:", err)
+}
+
+func writeToConnection(conn io.Writer, clipboardCh <-chan []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for data := range clipboardCh {
+		// Send data length as 4-byte big-endian
+		length := uint32(len(data))
+		header := make([]byte, 4)
+		binary.BigEndian.PutUint32(header, length)
+		// Send header
+		_, err := conn.Write(header)
+		if err != nil {
+			fmt.Println("Error writing header to connection:", err)
+			return
+		}
+		// Send data
+		_, err = conn.Write(data)
+		if err != nil {
+			fmt.Println("Error writing data to connection:", err)
+			return
+		}
 	}
 }
 
-func writeToConnection(conn io.Writer, ch <-chan []byte, wg *sync.WaitGroup) {
-	defer wg.Done()
-	writer := bufio.NewWriter(conn)
-	for text := range ch {
-		_, err := writer.Write(text)
-		if err != nil {
-			fmt.Println("Error writing to connection:", err)
-			return
-		}
-		_, err = writer.Write([]byte{'\n'})
-		if err != nil {
-			fmt.Println("Error writing newline to connection:", err)
-			return
-		}
-		writer.Flush()
+// Helper function to compare byte slices
+func equalBytes(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
 	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
