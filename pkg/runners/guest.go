00d15b36fe6a547ec221b0ea925afec857900e76
thelicato <thelicato@users.noreply.github.com>
2024-10-22 10:09:12 +0200

    feat: add initial dummy guest section

--- pkg/runners/guest.go
+++ pkg/runners/guest.go
@@ -1,7 +1,22 @@
 package runners
 
-import "github.com/thelicato/dqcs/pkg/logger"
+import (
+	"fmt"
+	"os"
+
+	"github.com/thelicato/dqcs/pkg/logger"
+	"github.com/thelicato/dqcs/pkg/utils"
+)
 
 func RunGuest() {
 	logger.Info("Running Guest component....")
+
+	conn, err := utils.OpenVirtioPort("guest")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening virtio port: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	utils.HandleConnection(conn)
 }
